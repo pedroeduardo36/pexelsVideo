98b8e8982401b2a1b8f4d7f2801f3d89ea3c575c
pedroeduardo36 <pedrovilarinho.gpd@gmail.com>
2023-12-04 10:28:43 -0300

    Initial Commit

---
+++ pexelsVideo/pexelsVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pexelsVideoApp.swift
+//  pexelsVideo
+//
+//  Created by Pedro Andrade on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct pexelsVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
